5a4b619e6e3c60d4b806b818c1f51e899de1c258
Lucas <lgrzina@identidad-digital.com>
2020-06-18 17:17:03 -0300

    a

---
+++ app/Http/Controllers/Admin/RegistradosController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Admin\CrudAdminController;
+use App\Http\Requests\Admin\CURegistradosRequest;
+use App\Repositories\RegistradosRepository;
+use Illuminate\Http\Request;
+use Prettus\Repository\Criteria\RequestCriteria;
+use Response;
+
+class RegistradosController extends CrudAdminController
+{
+    protected $routePrefix = 'registrados';
+    protected $viewPrefix  = 'admin.registrados.';
+    protected $actionPerms = 'registrados';
+
+    public function __construct(RegistradosRepository $repo)
+    {
+        $this->repository = $repo;
+
+        $this->middleware('permission:ver-'.$this->actionPerms, ['only' => ['index','filter','show']]);        
+        $this->middleware('permission:editar-'.$this->actionPerms, ['only' => ['create','store','edit','update','destroy']]);          
+    }
+
+    public function index()
+    {
+        parent::index();
+
+        return view($this->viewPrefix.'index')->with('data',$this->data);
+    }
+
+    public function filter(Request $request)
+    {
+        try
+        {
+            $this->repository->pushCriteria(new RequestCriteria($request));
+            $collection = $this->repository->paginate($request->get('per_page'))->toArray();        
+
+            $this->data = [
+                'list' => $collection['data'],
+                'paging' => array_only($collection,['total','current_page','last_page'])
+            ];   
+
+        }
+        catch (\Exception $ex) 
+        {
+            return $this->sendError($ex->getMessage(),500);
+        } 
+
+        return $this->sendResponse($this->data, trans('admin.success'));
+    }
+
+    public function show($id)
+    {
+        parent::show($id);
+
+        //$this->data['selectedItem']->load('xxx');
+
+        return view($this->viewPrefix.'show')->with('data', $this->data);
+    }
+
+    public function create()
+    {
+        parent::create();
+
+        data_set($this->data, 'selectedItem', [
+                'id' => 0
+        ]);
+
+        return view($this->viewPrefix.'cu')->with('data',$this->data);
+    }
+
+    public function store(CURegistradosRequest $request)
+    {
+        $model = $this->_store($request);
+        return $this->sendResponse($model,trans('admin.success'));        
+    }
+
+    public function edit($id)
+    {
+        parent::edit($id);
+
+        return view($this->viewPrefix.'cu')->with('data',$this->data);
+    }
+
+    public function update($id, CURegistradosRequest $request)
+    {
+        $model = $this->_update($id, $request);
+
+        return $this->sendResponse($model,trans('admin.success'));
+    }
+
+    public function exportXls(Request $request)
+    {
+        $this->repository->pushCriteria(new RequestCriteria($request));
+        
+        $data = $this->repository->all()->toArray();        
+        $name = 'Registrados';
+        $header = [
+            'id' => 'ID',
+            'nombre' => 'Nombre',
+            'pais' => 'Pais',
+        ];
+        $format = [
+        ];
+        return $this->_exportXls($data,$header,$format,$name);
+    }        
+}
