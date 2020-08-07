171e32a660655f72f37cf4c9cea4880686959572
Lucas <lgrzina@identidad-digital.com>
2020-08-07 16:49:29 -0300

    a

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -19,7 +19,7 @@
     <div class="row">
     	<div class="col-xs-12">
     		<h1>Bienvenido</h1>
-    		<p>Seleccioná una opción del menú de la izquierda para comenzar</p>	
+			<p>Seleccioná una opción del menú de la izquierda para comenzar. Hora actual: {{\Carbon\Carbon::now()->format('Y-m-d H:i:s')}}</p>	
     	</div>
 	</div>
     <div class="row">
