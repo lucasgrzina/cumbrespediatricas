d39c038a67764482ca0206e7040981de792ccaf7
Lucas <lgrzina@identidad-digital.com>
2020-08-07 19:24:55 -0300

    a

--- public/js/app.js
+++ public/js/app.js
@@ -46507,7 +46507,7 @@ var render = function() {
                       height: "100%"
                     },
                     attrs: {
-                      src: "https://player.vimeo.com/video/436601050",
+                      src: "https://player.vimeo.com/video/445697593",
                       frameborder: "0",
                       allow: "autoplay; fullscreen",
                       allowfullscreen: ""
@@ -46524,7 +46524,7 @@ var render = function() {
                       height: "100%"
                     },
                     attrs: {
-                      src: "https://player.vimeo.com/video/436601660",
+                      src: "https://player.vimeo.com/video/445697593",
                       frameborder: "0",
                       allow: "autoplay; fullscreen",
                       allowfullscreen: ""
