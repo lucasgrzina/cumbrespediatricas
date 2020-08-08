b6e5333ef496eb677e515efdfd66353f9b5ba1b4
Lucas <lgrzina@identidad-digital.com>
2020-08-08 09:43:14 -0300

    a

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -173,9 +173,14 @@ class HomeController extends AppBaseController
                 try {
                     $registrado = Registrado::where(\DB::raw('md5(id)'),$registradoGuid)->first();
                     \Log::info($registrado);
-                    $registrado->acciones()->whereNull('hasta')->orderBy('id','asc')->first()->update([
+                    $accion = $registrado->acciones()->whereNull('hasta')->orderBy('id','asc')->first();
+                    \Log::info($accion);
+                    $accion->hasta = Carbon::now()->format('Y-m-d H:i:s');
+                    \Log::info($accion);
+                    $accion->save();
+                    /*$registrado->acciones()->whereNull('hasta')->orderBy('id','asc')->first()->update([
                         'hasta' => Carbon::now()->format('Y-m-d H:i:s')
-                    ]);
+                    ]);*/
     
                 } catch (\Exception $e) {
                     \Log::info($e->getMessage());
