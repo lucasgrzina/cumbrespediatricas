15fee44edb8af7452beaa827528410d656951abe
Lucas <lgrzina@identidad-digital.com>
2020-09-16 12:33:58 -0300

    a

--- resources/views/front/danoneday/no-habilitado.blade.php
+++ resources/views/front/danoneday/no-habilitado.blade.php
@@ -1,7 +1,4 @@
 @extends('layouts.danoneday.front-vue')
-@section('head')
-<script src="https://www.google.com/recaptcha/api.js?render=6Leb2qYZAAAAALa7WyEDFhvJUYlYQH4Z_CJ-U-ie"></script>
-@endsection
 @section('content')
 <div class="container">            
     <div class="row">
