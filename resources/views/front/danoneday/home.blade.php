204add800b7aff482d40101f4d40f3601a8238ec
Lucas <lgrzina@identidad-digital.com>
2020-09-16 11:08:11 -0300

    a

--- resources/views/front/danoneday/home.blade.php
+++ resources/views/front/danoneday/home.blade.php
@@ -1,6 +1,21 @@
 @extends('layouts.danoneday.front')
 @section('head')
     @parent
+	<style>
+		body {
+			background-color: #f7fbfe!important;
+		}
+		.img-std {
+				width:100%;
+		}		
+		@media (max-width:1450px) {
+			.img-std {
+				width:100%;
+				max-width: 1150px;
+			}
+		}
+	</style>
+		
 	<script type="text/javascript">
 		var data = {!!json_encode($data)!!};
 		
@@ -8,11 +23,11 @@
 @endsection
 @section('content')
 <div class="row h-100">
-	<div class="col text-center justify-content-center align-self-center">
+	<div class="col text-center justify-content-center align-self-center my-auto">
 		@if($data['config']['etapa'] === 'F')
 			<!--img src="{{ asset('img/muchas graciasjpg-01.jpg') }}" style="width:100%;max-width:1024px;"/-->				
 		@else
-		<!--img src="{{ asset('img/std_similacmama_169.jpg') }}" style="width:100%;max-width:1024px;"/-->				
+		<img src="{{ asset('img/std_danoneday.jpg') }}" class="img-std"/>				
 		@endif
 	</div>	
 </div>
