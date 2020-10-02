2784fe447bd9d30438b3719860babd8bae645b56
Lucas <lgrzina@identidad-digital.com>
2020-10-02 11:48:22 -0300

    a

--- resources/views/layouts/forosas/front-vue.blade.php
+++ resources/views/layouts/forosas/front-vue.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta http-equiv="ScreenOrientation" content="autoRotate:disabled">
         <!--meta http-equiv="x-ua-compatible" content="IE=9" -->
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
