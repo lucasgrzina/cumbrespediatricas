88d0816e70ff514d73e32bce7f2ff120ffd22b5c
Lucas <lgrzina@identidad-digital.com>
2020-10-22 13:33:31 -0300

    a

--- app/Http/Controllers/IDController.php
+++ app/Http/Controllers/IDController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Registrado;
 use App\Helpers\CacheHelper;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Mail;
 
 class IDController extends Controller
@@ -28,12 +29,18 @@ class IDController extends Controller
 
     public function sendEmail() {
 
-            $pathToFile = asset('img/forosas/Confirmacion.jpg');
+        $registrados = Registrado::whereEvento('forosas')->where('created_at','<','2020-10-14')->pluck('nombre','email'); 
+        
+
+        
+        /*$pathToFile = asset('img/forosas/Confirmacion.jpg');
             $contenidoEmail = "Hola Lucas<br>".
             "Muchas gracias por registrarse al 7mo Foro de Salud Sustentable (SaS).<br>".
             "Ya puede darle un vistazo a la información del evento ingresando al sitio web www.foro-sas.com.ar";
             Mail::queue(new \App\Mail\RawMailable('lucasgrzina@gmail.com', 'Foro-Sas: Registro', $contenidoEmail));                    
+        */
 
+        return response()->json($registrados);
             
            
     }
