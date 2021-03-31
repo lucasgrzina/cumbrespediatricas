53623b47bef4574866531fa6b73fc8ff05bddd06
Lucas <lgrzina@identidad-digital.com>
2021-03-31 09:35:48 -0300

    a

--- app/Registrado.php
+++ app/Registrado.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class Registrado extends Authenticatable
 {
     public $table = 'registrados';
-
+    protected $rememberTokenName = false;
     protected $guard = 'web';
     public $fillable = [
         'nombre',
