37d4d374dbbc250687b68d5e7da64256267888d9
Lucas <lgrzina@identidad-digital.com>
2021-07-06 18:57:25 -0300

    a

--- resources/views/front/mentesinlimites/template.blade.php
+++ resources/views/front/mentesinlimites/template.blade.php
@@ -14,6 +14,8 @@
     
         <meta name="generator" content="AA v3.8.6">
         <title>Por una mente sin límites</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
 
         <link href="{{ url(mix('public/assets/mentesinlimites/css/styles.min.css')) }}" rel="stylesheet" type="text/css">
         <!-- Minified version of `es6-promise-auto` below. -->
